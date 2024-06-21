bb3294fa905294952588227908d5d131fed81925
Augusto <augustoazesc@gmail.com>
2024-06-21 19:09:39 -0300

    feat: How it works

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,37 +1,87 @@
-import Image from "next/image"
-export function HowItWorks() {
-    return (
-        <>
-            <div className="flex flex-col py-20">
-                <span id="how-it-works"></span>
-                <section className="container mx-auto py-20 text-center">
-                    <h5 className="text-2xl">Por que Energia Solar?</h5>
-                    <h4 className="text-3xl font-bold py-4">Investimento inteligente que ajuda o planeta</h4>
-                </section>
-                <section className="container mx-auto grid grid-cols-1 sm:grid-cols-3 text-xl px-4 gap-4">
-                    <div className="text-justify flex flex-col items-center bg-[#35b6aa33] py-8 px-8 rounded-3xl">
+import Image from "next/image";
 
-                        <Image src="/images/piggy-bank.svg" width={140} height={140} alt="porquinho" className="py-6" />
-                        <p className="">
-                            Desde 2012, a energia solar já gerou no Brasil mais de R$ 170 bilhões em investimentos. Um projeto bem dimensionado e corretamente instalado pode gerar um rendimento anual de até 40%, superando qualquer aplicação financeira atualmente disponível no mercado.
-                        </p>
-                    </div>
-                    <div className="text-justify flex flex-col items-center bg-[#35b6aa33] py-8 px-8 rounded-3xl">
-                        <Image src="/images/profits-analytics.svg" width={120} height={120} alt={"grafico"} className="py-6" />
-                        <p className="">
-                            Valorize sua propriedade! Milhões de pessoas já instalaram painéis solares nos seus telhados. Junte-se a esse movimento!
-                            Estudos apontam que um sistema de energia solar aumenta em 5,1% 0 valor de venda de um imóvel.
-                        </p>
-                    </div>
-                    <div className="text-justify flex flex-col items-center bg-[#35b6aa33] py-8 px-8 rounded-3xl">
-                        <Image src="/images/global-globe.svg" width={120} height={120} alt="globo" className="py-6" />
-                        <p className="">
-                            Valorize sua propriedade! Milhões de pessoas já instalaram painéis solares nos seus telhados. Junte-se a esse movimento!
-                            Estudos apontam que um sistema de energia solar aumenta em 5,1% 0 valor de venda de um imóvel.
-                        </p>
-                    </div>
-                </section>
+export function HowItWorks() {
+  return (
+    <>
+      <div className="flex flex-col py-4">
+        <span id="how-it-works"></span>
+        <section className="justify-between flex items-center container mx-auto py-20 text-center">
+          <div>
+            <h5 className="text-left text-4xl font-bold py-0">
+              Temos a melhor solução para você.
+            </h5>
+            <h4 className="text-left text-2xl py-4">
+              Conheça as diferentes formas de economizar que a Expertise
+              oferece:
+            </h4>
+          </div>
+          <a
+            href="#simulate-form"
+            className="bg-[#169576] text-white p-2 rounded-full px-8 items-center h-10 w-60"
+          >
+            SIMULAR ECONOMIA
+          </a>
+        </section>
+        <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 text-xl px-4 gap-16">
+          <div className="text-justify flex flex-col items-center bg-[#F5F5F5] rounded-3xl">
+            <div className="-mx-12 -mt-12">
+              <Image
+                src="/images/PainelSolar.png"
+                width={800}
+                height={341}
+                objectFit="cover"
+                alt="painelsolar"
+                className="py-6"
+              />
             </div>
-        </>
-    )
+            <div className="py-8 px-16">
+              <h5 className="text-left text-4xl font-bold py-0">
+                Geração de energia no seu imóvel
+              </h5>
+              <p className="font-bold text-2xl py-4">
+                Indicado para imóveis com área de telhado disponível para
+                instalação de painéis
+              </p>
+              <p>
+                A energia gerada nas placas fotovoltaicas é distribuída pela
+                rede elétrica da casa para ser utilizada imediatamente. O
+                excesso de energia gerada, que não é utilizado, pode ser enviado
+                de volta para a rede elétrica, fazendo o medidor girar para trás
+                e gerando créditos com a companhia elétrica.
+              </p>
+              <p className="py-4">
+                Com a energia solar suprindo parte ou toda a demanda da casa, a
+                conta de luz diminui significativamente.
+              </p>
+            </div>
+          </div>
+          <div className="text-justify flex flex-col items-center bg-[#F5F5F5] rounded-3xl">
+            <div className="-mx-12 -mt-12">
+              <Image
+                src="/images/UsinaSolar.png"
+                width={661}
+                height={341}
+                alt="usinasolar"
+                className="py-6"
+              />
+            </div>
+            <div className="py-8 px-16">
+              <h5 className="text-left text-4xl font-bold">
+                Geração de energia na Usina Solar
+              </h5>
+              <p className="font-bold text-2xl py-4">
+                Indicado para imóveis sem área de telhado disponível, como
+                apartamentos.
+              </p>
+              <p>A energia é produzida nas suas placas, instaladas
+                em um de nossos parques solares. Após a geração, é injetada na
+                rede da distribuidora de energia, assim, convertida em créditos
+                que são abatidos do consumo dos usuários do parque solar.</p>
+                <p className="py-4">Somos responsáveis por toda a operação, manutenção e garantia de eficiência das placas do parque.</p>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
